e654545ba58f75cd19c64e8aacffb109e9a522f1
rveloz <jorge_pw@hotmail.es>
2019-03-07 20:06:21 -0600

    cambio de json a jsonp

--- routes/api/quiebre.js
+++ routes/api/quiebre.js
@@ -17,9 +17,9 @@ var router = express.Router();
         mysqlModel.getQuiebre((err,data)=>{
             if(err){
                 console.log(err);                
-                return res.status(500).json({error:"Algo paso"});
+                return res.status(500).jsonp({error:"Algo paso"});
             }
-            res.status(200).json(data);
+            res.status(200).jsonp(data);
         });
     }); //get//
 
